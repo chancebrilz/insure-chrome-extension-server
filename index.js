ec388918117879309ed294756c8654b25f841e09
chancebrilz <chancebrilz@gmail.com>
2019-03-08 11:35:32 -0500

    look for ip qp

--- index.js
+++ index.js
@@ -3,7 +3,16 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.json(req);
+  if ("ip" in req.query) {
+    return res.json({
+      ip: req.query.ip,
+      status: "unknonw"
+    });
+  } else {
+    return res.json({
+      error: "No IP address supplied"
+    });
+  }
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
