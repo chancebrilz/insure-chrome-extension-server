337f62b1271f4e950f36488319cf82ea3644a19b
chancebrilz <chancebrilz@gmail.com>
2019-03-08 14:23:43 -0500

    pulling correct model

--- src/db.js
+++ src/db.js
@@ -5,6 +5,10 @@ const connectionUrl = config.db.connection;
 
 mongoose.connect(connectionUrl, { useNewUrlParser: true });
 
+mongoose.connection.on("open", function() {
+  console.log("Connected to mongoose");
+});
+
 const BlacklistSchema = new mongoose.Schema({
   url: String,
   malicious: Boolean
