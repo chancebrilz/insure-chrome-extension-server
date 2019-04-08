9f9e747addc4f55f651ce954c83393fcfee363d5
Deep Patel <35742688+deepsworld@users.noreply.github.com>
2019-04-08 09:11:55 -0400

    Update getDB.py

--- getDB.py
+++ getDB.py
@@ -8,11 +8,11 @@ config.read('config.ini')
 
 
 def update_db():
-    """Download the PhishTank URLs database in lib/files/database_phishtank.json."""
+    """Download the PhishTank URLs database"""
     api_key = config.get('phishtank', 'api_key')
     api_url = 'http://data.phishtank.com/data/%s/online-valid.json.bz2' % (api_key)
     compraw = urllib.request.urlopen(api_url).read()
     rawdecomp = bz2.decompress(compraw)
     database = json.loads(rawdecomp.decode('utf-8'))
-    with open('lib/files/database_phishtank.json', 'w') as outfile:
+    with open('database_phishtank.json', 'w') as outfile:
         json.dump(database, outfile)
